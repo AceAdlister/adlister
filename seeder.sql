a8f338dea03f5d200a1a5ff5539be1820eafbcf8
Erik <erikrhansen@ricealumni.net>
2017-10-25 14:28:41 -0500

    Fixed development server user seeder

--- seeder.sql
+++ seeder.sql
@@ -1,10 +1,10 @@
 insert into users(username, email, password)
 values
-('admin','SuperUser@Domain','hash1'),
-('root', 'SQLInjector@Domain','hash2'),
-('Mary Sue','SuperSnowflake@MyDomain','hash3'),
-('Larry Milam','TruckOn@domain','hash4'),
-('Ben Foreman','ConstructorJobs@domain','hash5');
+('admin','SuperUser@Domain','$2a$12$5WhkCPBhE7bg/Cf/Fgizeelm5E0T6tLTUfyCEgXCe9kj5PJJBjlF2'),#LockDown
+('root', 'SQLInjector@Domain','$2a$12$tTP.kzqOhnhgj5XcrAby4uGnabHlRIo6lnB7Hj3oafgMULv6bSQee'),#L33T
+('Mary Sue','SuperSnowflake@MyDomain','$2a$12$9S4mhP.Qb3vhRWQ8txV.ouzK98v1bUSkccGnvGxPzXmPyubHedDUm'),#2Perfect
+('Larry Milam','TruckOn@domain','$2a$12$jYNvZ97.YKePri5hySkPCOaMGFsgbwQ.ueuO37pnNE/vm8b1w1IlW'),#Texas4Ever
+('Ben Foreman','ConstructorJobs@domain','$2a$12$onFSEe.5M5usOKbdIjThRulRZSj3xc.by6WMUMhB7RKgD3YWOR0H.');#BuildIt
 
 insert into ads(user_id, title, description)
 VALUES
@@ -13,3 +13,5 @@ VALUES
 (4,'Getting rid of a couch','You can either come get it for free, or I\'ll bring it in my truck for $40'),
 (5,'Build your dream home!', 'At Foreman Construction we put you in the last house you ever need!'),
 (1,'Anouncment', 'Please stop advertising waffles on our site');
+
+#
