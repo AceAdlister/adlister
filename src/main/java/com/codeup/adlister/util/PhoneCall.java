04b74e39658d77611a5ee73dddbfe3c2c3830518
chasehammock <chasehammock@gmail.com>
2017-10-27 10:35:49 -0500

    added files for the twilio SMS and PhoneCall in the util folder

--- src/main/java/com/codeup/adlister/util/PhoneCall.java
+++ src/main/java/com/codeup/adlister/util/PhoneCall.java
@@ -1,4 +1,4 @@
-package com.codeup.adlister.controllers;
+package com.codeup.adlister.util;
 
         import com.twilio.Twilio;
         import com.twilio.rest.api.v2010.account.IncomingPhoneNumber;
