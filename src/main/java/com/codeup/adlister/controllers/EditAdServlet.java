51a3a8cc211ec1cf39d523e166da027b7d5c00a4
Erik <erikrhansen@ricealumni.net>
2017-10-30 10:26:06 -0500

    made servlets for the creation of ads and the editing of them.

---
+++ src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
@@ -0,0 +1,18 @@
+package com.codeup.adlister.controllers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "EditAdServlet",urlPatterns = "/edit_ad")
+public class EditAdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+}
