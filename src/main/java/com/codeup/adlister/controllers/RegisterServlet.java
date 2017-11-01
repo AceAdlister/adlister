450aad52acb02d3208754562e3290f2264d1d1c8
chasehammock <chasehammock@gmail.com>
2017-11-01 09:38:19 -0500

    added the register servlet

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -44,82 +44,82 @@ public class RegisterServlet extends HttpServlet {
 //            || password.isEmpty()
 //            || (!password.equals(passwordConfirmation));
 
-      HashMap<String, String> errors= new HashMap<>();
+    HashMap<String, String> errors= new HashMap<>();
 //      Set up tags
-      if(username ==null || username.equalsIgnoreCase("")){
-          errors.put("username","The username is empty.");
-      }
+    if(username ==null || username.equalsIgnoreCase("")){
+      errors.put("username","The username is empty.");
+    }
 
-      if(firstName==null ||firstName.equalsIgnoreCase("")){
-          errors.put("firstName","The First Name is empty.");
-      }
+    if(firstName==null ||firstName.equalsIgnoreCase("")){
+      errors.put("firstName","The First Name is empty.");
+    }
 
-      if(lastName==null || lastName.equalsIgnoreCase("")){
-          errors.put("lastName","The Last Name is empty.");
-      }
+    if(lastName==null || lastName.equalsIgnoreCase("")){
+      errors.put("lastName","The Last Name is empty.");
+    }
 
-      if(email ==null || email.equalsIgnoreCase("")){
-          errors.put("email","The email is empty.");
-      }else if (!email.contains("@")){
-          errors.put("email","The email does not have a valid domain.");
-      }
+    if(email ==null || email.equalsIgnoreCase("")){
+      errors.put("email","The email is empty.");
+    }else if (!email.contains("@")){
+      errors.put("email","The email does not have a valid domain.");
+    }
 
-      if(streetAddress==null || streetAddress.equalsIgnoreCase("")){
-          errors.put("address","The Street Address is empty.");
-      }
+    if(streetAddress==null || streetAddress.equalsIgnoreCase("")){
+      errors.put("address","The Street Address is empty.");
+    }
 
-      if(state==null || state.equalsIgnoreCase("")){
-          errors.put("state","The State was not selected");
-      }
+    if(state==null || state.equalsIgnoreCase("")){
+      errors.put("state","The State was not selected");
+    }
 
-      if(zipcode==null || zipcode.equalsIgnoreCase("")){
-          errors.put("zipcode","The Zipcode is empty.");
-      }else if(zipcode.length()!=5 && isNotNumeric(zipcode)){
-          errors.put("zipcode","The Zipcode is not 5 numbers.");
-      }
+    if(zipcode==null || zipcode.equalsIgnoreCase("")){
+      errors.put("zipcode","The Zipcode is empty.");
+    }else if(zipcode.length()!=5 && isNotNumeric(zipcode)){
+      errors.put("zipcode","The Zipcode is not 5 numbers.");
+    }
 
-      if(phone==null || phone.equalsIgnoreCase("")){
-          errors.put("phone","The Phone Number is empty.");
-      }else if(phone.length()!=10 && isNotNumeric(phone)){
-          errors.put("phone","The Phone Number is not 10 numbers.");
-      }
+    if(phone==null || phone.equalsIgnoreCase("")){
+      errors.put("phone","The Phone Number is empty.");
+    }else if(phone.length()!=10 && isNotNumeric(phone)){
+      errors.put("phone","The Phone Number is not 10 numbers.");
+    }
 
-      if(password==null ||password.equalsIgnoreCase("")){
-          errors.put("password","The Password is empty");
-      }
+    if(password==null ||password.equalsIgnoreCase("")){
+      errors.put("password","The Password is empty");
+    }
 
-      if(!password.equals(passwordConfirmation)){
-          errors.put("nomatch","The Passwords do not match.");
-      }
+    if(!password.equals(passwordConfirmation)){
+      errors.put("nomatch","The Passwords do not match.");
+    }
 
 
 
 //      forward tags
-      if(errors.size()!=0) {
-        request.setAttribute("errors", errors);
+    if(errors.size()!=0) {
+      request.setAttribute("errors", errors);
       request.getRequestDispatcher("/WEB-INF/register.jsp")
-                .forward(request, response);
-      }else {
+              .forward(request, response);
+    }else {
 
 //         encrypt / hash the password
 
-        int numberOfRounds = 12;
-        String hash = BCrypt.hashpw(password, BCrypt.gensalt(numberOfRounds));
+      int numberOfRounds = 12;
+      String hash = BCrypt.hashpw(password, BCrypt.gensalt(numberOfRounds));
 
 //         create and save a new user
-        User user = new User(username, email, firstName, lastName, streetAddress, state, zipcode, phone, hash);
-        DaoFactory.getUsersDao().insert(user);
-        response.sendRedirect("/login");
-      }
+      User user = new User(username, email, firstName, lastName, streetAddress, state, zipcode, phone, hash);
+      DaoFactory.getUsersDao().insert(user);
+      response.sendRedirect("/login");
+    }
 
   }
 
-    private boolean isNotNumeric(String zipcode) {
-      try{
-          int result=Integer.parseInt(zipcode);
-          return false;
-      }catch(NumberFormatException e){
-          return true;
-      }
+  private boolean isNotNumeric(String zipcode) {
+    try{
+      int result=Integer.parseInt(zipcode);
+      return false;
+    }catch(NumberFormatException e){
+      return true;
     }
+  }
 }
