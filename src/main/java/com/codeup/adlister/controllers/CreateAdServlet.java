84e09144d569e8aa8685d007ef306f8e7dca36b1
Erik <erikrhansen@ricealumni.net>
2017-11-01 10:35:44 -0500

    Fixed errors in the MySQL query for creating ads. There were to few fields.
    Added validation messages to create ads page.
    
    Fixed the css problem for states here. Will also do on register page.

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 @WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
@@ -23,23 +24,47 @@ public class CreateAdServlet extends HttpServlet {
             .forward(request, response);
   }
 
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
     User sessionUser =(User) request.getSession().getAttribute("user");
     if (sessionUser == null) {
         response.sendRedirect("/login");
         return;
     }
-    User user=DaoFactory.getUsersDao().findByUsername(sessionUser.getUsername());
+      HashMap<String, String> errors=new HashMap<>();
+        User user=DaoFactory.getUsersDao().findByUsername(sessionUser.getUsername());
+           Long userID = user.getId(); // for now we'll hardcode the user id
+           String title =request.getParameter("title");
+           String description= request.getParameter("description");
+           String address= request.getParameter("address");
+           String state= request.getParameter("state");
+           String zip_code= request.getParameter("zip_code");
 
-    Ad ad = new Ad(
-            user.getId(), // for now we'll hardcode the user id
-            request.getParameter("title"),
-            request.getParameter("description"),
-            request.getParameter("address"),
-            request.getParameter("state"),
-            request.getParameter("zip_code")
-    );
+           if(title.isEmpty()){
+               errors.put("title","The title is empty");
+           }
+          if(description.isEmpty()){
+              errors.put("description","The description is empty");
+          }
+
+          if(address.isEmpty()){
+              errors.put("address","The address is empty");
+          }
+        if(state.isEmpty()){
+          errors.put("state","The state is empty");
+        }
+      if(zip_code.isEmpty()){
+          errors.put("zip_code","The zip_code is empty");
+      }
+
+    if(errors.size()!=0){
+        request.setAttribute("errors", errors);
+        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+                .forward(request, response);
+    }else{
+    Ad ad = new Ad(userID,title,description,address,state,zip_code);
     DaoFactory.getAdsDao().insert(ad);
     response.sendRedirect("/ads");
+
+    }
   }
 }
